3a0629f1739c8e3a1988563ae745b8d54e0dab4a
Dmitry <academik2006@yandex.ru>
2025-12-25 15:51:19 +0700

    получил фото с пользователя

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from telebot import types
 import telebot
 import asyncio
 import time
+import io
 from datetime import datetime
 from threading import Thread
 from datetime import timedelta
 from promotions import promotions  
+from telebot.types import InputMediaPhoto
 from api_key import API_TOKEN
 import logging
 from PIL import Image, ImageDraw, ImageFont
@@ -19,6 +21,9 @@ bot.delete_webhook()
 
 logging.basicConfig(level=logging.INFO)  
 logger = logging.getLogger(__name__)
+
+# Словарь для временного хранения данных пользователей
+user_data = {}
 
 
 async def main():    
@@ -62,13 +67,23 @@ def add_user_on_start(message):
     else:
         check_character_and_send_status(user_id)    
 
+
+def create_keyboard_for_choose_avatar_photo ():
+
+    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    load_button = types.InlineKeyboardButton(text="Загрузить своё", callback_data='load_own')
+    select_standard_button = types.InlineKeyboardButton(text="Выбрать стандартное", callback_data='select_standard')
+    keyboard.add(load_button, select_standard_button)
+    
+    return keyboard
+
 def create_keyboard_for_choose_gender ():
 
     buttons = [
         'Мужской',
         'Женский'        
     ]
-    return create_keyboard (buttons)    
+    return create_keyboard (buttons, True)    
 
 def create_keyboard_for_new_user():
 
@@ -77,11 +92,11 @@ def create_keyboard_for_new_user():
         'Условия акции',
         'Создать персонажа',
     ]
-    return create_keyboard (buttons)
+    return create_keyboard (buttons, False)
     
 
-def create_keyboard(buttons):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)    
+def create_keyboard(buttons, one_time):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2, one_time_keyboard=one_time)    
     for text in buttons:
         btn = types.KeyboardButton(text=text)
         keyboard.add(btn)
@@ -96,36 +111,126 @@ def handle_buttons(message):
     
     if text in ["мужской", "женский"]:
         gender = "male" if text == "мужской" else "female"
-        create_character(user_id, gender)
-    elif text.startswith("создать персонажа"):
-        bot.send_message(user_id, "Выбери пол своего персонажа:", reply_markup=create_keyboard_for_choose_gender())        
+        user_data[message.chat.id] = {"gender": gender}        
+        bot.send_message(message.chat.id, "Теперь выбери имя своего персонажа (не более 30 символов, буквы и цифры).")
+        bot.register_next_step_handler(message, process_character_name)
+    elif text.startswith("создать персонажа"):        
+        bot.send_message(user_id, "Выбери пол своего персонажа", reply_markup=create_keyboard_for_choose_gender())        
     elif text.startswith("кормление"):
-        update_character_parameter(user_id, 'hunger', +10)
+        ugrade_character_parameter_and_show_new_avatar(user_id, 'hunger', +10)        
     elif text.startswith("посещение"):
-        update_character_parameter(user_id, 'entertainment', 5)
-    elif text.startswith("шопинг") or text.startswith("провести время с друзьями"):
-        update_character_parameter(user_id, 'money_needs', +5)
+        ugrade_character_parameter_and_show_new_avatar(user_id, 'entertainment', +5)                
+    elif text.startswith("шопинг"):
+        ugrade_character_parameter_and_show_new_avatar(user_id, 'money_needs', +5)
+    elif text.startswith("провести время с друзьями"):
+        ugrade_character_parameter_and_show_new_avatar(user_id, 'money_needs', +5)    
     elif text.startswith("угощение"):
-        update_character_parameter(user_id, 'entertainment', +5)
+        ugrade_character_parameter_and_show_new_avatar(user_id, 'hunger', +10) 
     elif text.startswith("перевод денег"):
-        update_character_parameter(user_id, 'money_needs', +5)
+        ugrade_character_parameter_and_show_new_avatar(user_id, 'money_needs', +5)
     elif text.startswith("встреча с работы"):
-        update_character_parameter(user_id, 'entertainment', +5)
+        ugrade_character_parameter_and_show_new_avatar(user_id, 'fatigue', +5) 
     elif text.startswith("предоставление возможности"):
-        update_character_parameter(user_id, 'entertainment', +5)
+        ugrade_character_parameter_and_show_new_avatar(user_id, 'entertainment', +5)         
     else:
         pass        
 
-def create_character(user_id, gender):    
-    # Генерируем стандартный аватар
-    img = generate_avatar(gender)
-    bio = img.getvalue()  # Получаем байтовое представление изображения
+
+def ugrade_character_parameter_and_show_new_avatar (user_id, param_name, value_change):
+    update_character_parameter(user_id, param_name, value_change)
+    check_character_and_send_status(user_id)
+
+def process_character_name(message):
+    name = message.text.strip()
+    if not is_valid_name(name):
+        bot.reply_to(message, "Имя некорректно. Повторите попытку.")
+        return
     
-    # Добавляем персонажа в базу
-    name = f"{gender.capitalize()} #{random.randint(1000, 9999)}"
-    add_character_to_database(user_id, name, gender, bio)    
+    user_data[message.chat.id]["name"] = name
+    bot.send_message(message.chat.id, "Выбери фото для персонажа", reply_markup=create_keyboard_for_choose_avatar_photo())
+
+# Функция для проверки правильности имени
+def is_valid_name(name):
+    """Проверяет длину имени и наличие спецсимволов."""
+    return len(name.strip()) <= 30 and all(char.isalnum() or char.isspace() for char in name) 
+
+@bot.callback_query_handler(func=lambda call: call.data == 'load_own')
+def handle_load_own(call):
+    chat_id = call.message.chat.id
+    bot.send_message(chat_id, "Отправьте вашу фотографию.")
+    bot.register_next_step_handler_by_chat_id(chat_id, process_user_photo)
+
+def process_user_photo(message):
+    if message.content_type != 'photo':
+        bot.reply_to(message, "Это не фотография. Пожалуйста, отправьте фото.")
+        return
     
-    bot.send_message(user_id, f"Персонаж {name} успешно создан!")
+    # Берём самую большую версию фотографии
+    file_info = bot.get_file(message.photo[-1].file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    
+    # Компрессия изображения до 150х150 пикселей
+    from PIL import Image
+    image = Image.open(io.BytesIO(downloaded_file)).resize((150, 150))
+    compressed_image_bytes = io.BytesIO()
+    image.save(compressed_image_bytes, format='PNG')  # Сохраняем сжатое изображение в памяти
+    compressed_image_bytes.seek(0)
+    
+    # Сохраняем изображение в словаре user_data
+    user_data[message.chat.id]['photo'] = compressed_image_bytes.read()
+    bot.send_message(message.chat.id, "Фотография принята.", reply_markup=types.ReplyKeyboardRemove())
+    # Переходим к созданию персонажа
+    create_character(message.chat.id)   
+
+@bot.callback_query_handler(func=lambda call: call.data == 'select_standard')
+def handle_select_standard(call):
+    chat_id = call.message.chat.id
+    gender = user_data[chat_id]['gender']
+    images = []
+    buttons = []
+    
+    # Показ стандартных изображений
+    for i in range(3):
+        filename = f'men_{i}.png' if gender == 'male' else f'girl_{i}.png'
+        with open(filename, 'rb') as f:
+            img_data = f.read()
+            
+        button_text = str(i+1)
+        buttons.append(types.InlineKeyboardButton(button_text, callback_data=f'select:{button_text}'))
+        images.append(img_data)
+    
+    # Формируем inline-клавиатуру с номерами картинок
+    keyboard = types.InlineKeyboardMarkup().add(*buttons)
+    bot.send_media_group(chat_id, [InputMediaPhoto(image) for image in images])
+    bot.send_message(chat_id, "Выберите одну из фотографий:", reply_markup=keyboard)
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('select:'))
+def handle_select_standard_photo(call):
+    selected_number = int(call.data.split(':')[1]) - 1  # Преобразование номера в индекс массива
+    chat_id = call.message.chat.id
+    gender = user_data[chat_id]['gender']
+    filename = f'men_{selected_number}.png' if gender == 'male' else f'girl_{selected_number}.png'
+    
+    with open(filename, 'rb') as f:
+        user_data[chat_id]['photo'] = f.read()
+    
+    bot.answer_callback_query(call.id, show_alert=False, text="Фото выбрано.")
+    create_character(chat_id)
+
+
+def create_character(user_id):    
+    data = user_data.pop(user_id)
+    gender = data['gender']
+    name = data.get('name', None)  # Если имя ещё не задано, оставляем None
+    photo_blob = data.get('photo', None)
+    
+    # Генерация стандартного имени, если имя не было предварительно определено
+    if not name:
+        name = f"{gender.capitalize()} #{random.randint(1000, 9999)}"
+    
+    # Добавление персонажа в базу данных
+    add_character_to_database(user_id, name, gender, photo_blob)            
+    bot.send_message(user_id, f"Персонаж успешно создан!")
     check_character_and_send_status(user_id)  
 
 def check_character_and_send_status(user_id):
@@ -146,18 +251,9 @@ def check_character_and_send_status(user_id):
     else:
         buttons.extend(["Посещение футбольного матча", "Угощение домашним обедом", "Встреча с работы", "Проведение времени с друзьями"])
     
-    keyboard = telebot.types.ReplyKeyboardMarkup(row_width=2)
-    for button_text in buttons:
-        keyboard.add(button_text)
+    keyboard = create_keyboard(buttons, False)
     
     send_character_image(user_id, char_id, name, gender, hunger, fatigue, entertain, money_need, total_state, keyboard)
-
-def send_character_image(user_id, char_id, name, gender, hunger, fatigue, entertain, money_need, total_state, keyboard=None):
-    img_bytes = draw_character(char_id, name, gender, hunger, fatigue, entertain, money_need, total_state)
-    bio = BytesIO(img_bytes)
-    bio.seek(0)
-    bot.send_photo(user_id, bio, caption=f"{name}\nHunger: {hunger:.0f}%\nFatigue: {fatigue:.0f}%\nEntertainment: {entertain:.0f}%\nMoney Needs: {money_need:.0f}%",
-                  reply_markup=keyboard)
 
 def generate_avatar(gender):
     width, height = 200, 200
@@ -166,19 +262,50 @@ def generate_avatar(gender):
     font = ImageFont.truetype("arial.ttf", size=20)
     
     text = f'{gender.capitalize()} Avatar'
-    w, h = font.getbbox(text)[2:]  # Возвращает ширину и высоту текста
+    w, h = font.getbbox(text)[2:]
     x = (width - w) / 2
     y = (height - h) / 2
-    draw.text((x,y), text, fill='black', font=font)
+    draw.text((x, y), text, fill='black', font=font)
     
     bio = BytesIO()
     img.save(bio, format='PNG')
-    return bio
+    return bio.getvalue()
 
-def draw_character(char_id, name, gender, hunger, fatigue, entertain, money_need, total_state):
-    # Генерация изображения персонажа (можно заменить на реальные рендеры или анимации)
-    img = generate_avatar(gender)
-    return img.getvalue()      
+def send_character_image(user_id, char_id, name, gender, hunger, fatigue, entertain, money_need, total_state, keyboard=None):
+    conn = sqlite3.connect('users.db')  # Подключение к базе данных
+    cursor = conn.cursor()
+    
+    # Получаем изображение персонажа из базы данных
+    cursor.execute("SELECT photo FROM characters WHERE user_id=?", (user_id,))
+    row = cursor.fetchone()
+    
+    if row is None:
+        # Если персонажа нет в базе данных, генерируем аватар
+        img_bytes = generate_avatar(gender)
+    elif row[0] is None:
+        # Если изображение отсутствует в базе данных, тоже генерируем аватар
+        img_bytes = generate_avatar(gender)
+    else:
+        img_bytes = row[0]  # Получаем BLOB-данные изображения
+    
+    # Закрываем соединение с базой данных
+    conn.close()
+    
+    # Готовим изображение для отправки
+    bio = BytesIO(img_bytes)
+    bio.seek(0)
+    
+    # Формирование подписи к картинке
+    caption = (
+        f"{name}\n"
+        f"Голод: {hunger:.0f}%\n"
+        f"Усталость: {fatigue:.0f}%\n"
+        f"Потребность в развлечениях: {entertain:.0f}%\n"
+        f"Необходимость в финансах: {money_need:.0f}%"
+    )
+    
+    # Отправляем изображение
+    bot.send_photo(user_id, bio, caption=caption, reply_markup=keyboard)      
 
   
 def get_users():
